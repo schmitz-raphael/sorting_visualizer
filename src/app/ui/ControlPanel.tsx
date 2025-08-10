d185f264b23f2979fff2974ee7d37d7c9d4adb4a
Raphael  Schmitz <raphael.schmitz276@gmail.com>
2025-08-10 15:51:04 +0200

    change slider values + add reset button again

--- src/app/ui/ControlPanel.tsx
+++ src/app/ui/ControlPanel.tsx
@@ -12,12 +12,11 @@ export default function ControlPanel({ nums, setNums, setHighlighted}: ControlPa
     const [isSorting, setIsSorting] = useState(false);
     const [selectedAlgorithm, setSelectedAlgorithm] = useState("bubble");
     const [comparisons, setComparisons] = useState<number>(0);
-    const [arraySize, setArraySize] = useState<number>(20);
-    const [delay, setDelay] = useState<number>(0.5);
+    const [arraySize, setArraySize] = useState<number>(50);
+    const [delay, setDelay] = useState<number>(500);
 
     const startSorting = async () => {
-      if (isSorting) return;
-      resetArray();
+      
       setIsSorting(true);
       let sortingFactory = new SortingFactory(nums, setNums, setHighlighted, incrementComparisons, delay);
       switch (selectedAlgorithm){
@@ -27,16 +26,28 @@ export default function ControlPanel({ nums, setNums, setHighlighted}: ControlPa
         case "selection":
           await sortingFactory.selectionSort();
           break;
+        case "insertion":
+          await sortingFactory.insertionSort();
+          break;
+        case "merge":
+          await sortingFactory.mergeSort(); 
+          break;
+        case "quick":
+          await sortingFactory.quickSort();
+          break;
+        case "heap":
+          await sortingFactory.heapSort();
+          break;
         default:
           break;
       }
-      setIsSorting(false);
     };
 
     const resetArray = () => {
       setNums(createArray(arraySize));
       setHighlighted([]);
       setComparisons(0);
+      setIsSorting(false);
     }
     const changeAlgorithm = (e: React.ChangeEvent<HTMLSelectElement>) => {
       setSelectedAlgorithm(e.target.value);
@@ -48,7 +59,7 @@ export default function ControlPanel({ nums, setNums, setHighlighted}: ControlPa
       resetArray();
     }
     const changeDelay = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const newDelay = Number(e.target.value) / 100;
+      const newDelay = Number(e.target.value);
       setDelay(newDelay); 
     }
     const incrementComparisons = () => {
@@ -60,7 +71,7 @@ export default function ControlPanel({ nums, setNums, setHighlighted}: ControlPa
           <select 
             className = "selectedAlgorithm bg-white border border-gray-300 rounded p-2 mr-4" 
             onChange={changeAlgorithm}
-            disabled={isSorting}
+
           >
               <option value="bubble">Bubble Sort</option>
               <option value="selection">Selection Sort</option>
@@ -70,26 +81,32 @@ export default function ControlPanel({ nums, setNums, setHighlighted}: ControlPa
               <option value="heap">Heap Sort</option>
           </select>
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded"
+            className="startSorting bg-blue-500 text-white px-4 py-2 rounded"
             onClick={startSorting}
             disabled={isSorting}
           >
             Start Sort
           </button>
+          <button
+            className="bg-blue-500 mx-2 text-white px-4 py-2 rounded"
+            onClick={resetArray}
+          >
+            Reset Array
+          </button>
           <p className="m-2">Array Size: {arraySize}</p>
           <input 
               type="range"
               min = "10"
-              max = "30"
-              defaultValue="20"
+              max = "100"
+              defaultValue="50"
               onChange={changeArraySize}/>
-          <p className="m-2">Delay (in seconds): {delay}</p>
+          <p className="m-2">Delay (ms): {delay}</p>
           <input 
               className="mx-2"
               type="range"
-              min = "10"
-              max = "100"
-              defaultValue="50"
+              min = "1"
+              max = "1000"
+              defaultValue="500"
               onChange={changeDelay}/>
         </div>
         <p className="m-2">Number of comparisons: {comparisons}</p>
